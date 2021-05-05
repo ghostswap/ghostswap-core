393985c0ac7cba1025a24066435f8e141ee89a11
ghostswap <83729862+ghostswap@users.noreply.github.com>
2021-05-05 16:29:17 -0400

    Update and rename SPIRITToken.sol to GHOSTToken.sol

--- GHOSTToken.sol
+++ GHOSTToken.sol
@@ -653,13 +653,13 @@ contract BEP20 is Context, IBEP20, Ownable {
     }
 }
 
-// File: contracts\SPIRITToken.sol
+// File: contracts\GHOSTToken.sol
 
 pragma solidity 0.6.12;
 
 
-// SpiritToken with Governance.
-contract SpiritToken is BEP20('SpiritSwap Token', 'SPIRIT') {
+// Ghost Token with Governance.
+contract GhostToken is BEP20('GhostSwap Token', 'GHOST') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
@@ -768,9 +768,9 @@ contract SpiritToken is BEP20('SpiritSwap Token', 'SPIRIT') {
         );
 
         address signatory = ecrecover(digest, v, r, s);
-        require(signatory != address(0), "SPIRIT::delegateBySig: invalid signature");
-        require(nonce == nonces[signatory]++, "SPIRIT::delegateBySig: invalid nonce");
-        require(now <= expiry, "SPIRIT::delegateBySig: signature expired");
+        require(signatory != address(0), "GHOST::delegateBySig: invalid signature");
+        require(nonce == nonces[signatory]++, "GHOST::delegateBySig: invalid nonce");
+        require(now <= expiry, "GHOST::delegateBySig: signature expired");
         return _delegate(signatory, delegatee);
     }
 
@@ -800,7 +800,7 @@ contract SpiritToken is BEP20('SpiritSwap Token', 'SPIRIT') {
         view
         returns (uint256)
     {
-        require(blockNumber < block.number, "SPIRIT::getPriorVotes: not yet determined");
+        require(blockNumber < block.number, "GHOST::getPriorVotes: not yet determined");
 
         uint32 nCheckpoints = numCheckpoints[account];
         if (nCheckpoints == 0) {
@@ -837,7 +837,7 @@ contract SpiritToken is BEP20('SpiritSwap Token', 'SPIRIT') {
         internal
     {
         address currentDelegate = _delegates[delegator];
-        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying SPIRITs (not scaled);
+        uint256 delegatorBalance = balanceOf(delegator); // balance of underlying GHOSTs (not scaled);
         _delegates[delegator] = delegatee;
 
         emit DelegateChanged(delegator, currentDelegate, delegatee);
@@ -873,7 +873,7 @@ contract SpiritToken is BEP20('SpiritSwap Token', 'SPIRIT') {
     )
         internal
     {
-        uint32 blockNumber = safe32(block.number, "SPIRIT::_writeCheckpoint: block number exceeds 32 bits");
+        uint32 blockNumber = safe32(block.number, "GHOST::_writeCheckpoint: block number exceeds 32 bits");
 
         if (nCheckpoints > 0 && checkpoints[delegatee][nCheckpoints - 1].fromBlock == blockNumber) {
             checkpoints[delegatee][nCheckpoints - 1].votes = newVotes;
